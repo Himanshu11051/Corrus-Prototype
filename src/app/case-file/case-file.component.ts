00fb21c122b4722ca8c2809c6e6fa214f8be09bc
Himanshu Kumar <himanshu.kumar@newgen.co.in>
2017-09-29 16:35:15 +0530

    Case File Case Details Tab

--- src/app/case-file/case-file.component.ts
+++ src/app/case-file/case-file.component.ts
@@ -14,5 +14,20 @@ export class CaseFileComponent implements OnInit {
   currentTab = 'OVERVIEW';
   switchTab(currentTab){
     this.currentTab = currentTab;
-  }
+  };
+  showComposer = true;
+  showTaskList = false;
+  composerToggleText = 'Hide Composer';
+  composerToggleBtnIcon = 'remove';
+  toggelTaskComposer(){
+    this.showComposer = ! this.showComposer;
+    if(this.showComposer){
+      this.composerToggleText = 'Hide Composer';
+      this.composerToggleBtnIcon = 'remove';
+    }
+    else{
+      this.composerToggleText = 'Show Composer';
+      this.composerToggleBtnIcon = 'add';
+    }
+  };
 }
